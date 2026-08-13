f8df43e8ec3a1bd5c91fc350f6d720acf1d3b08f
Stephen Bigelow <freshbugs@gmail.com>
2026-08-13 13:39:02 -0700

    microscopically less dumb status popups

--- tinywl.c
+++ tinywl.c
@@ -201,10 +201,13 @@ static void handle_run(struct tinywl_server *server) {
     spawn("cmd=$(zenity --entry) && eval 'exec $cmd'");
 }
 
-static void handle_status(struct tinywl_server *server) {
-    spawn("d=$(date +'%H:%M')$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage) && zenity --info --text=\"$d\"");
+static void handle_battery(struct tinywl_server *server) {
+    spawn("upower -d | grep -m 1 percentage | zenity --info --text=\"$(cat)\"");
 }
 
+static void handle_time(struct tinywl_server *server) {
+    spawn("zenity --info --text=\"$(date)\"");
+}
 
 static void handle_volume_up(struct tinywl_server *server) {
     /* Increase volume by 5%, capping at 100% */
@@ -250,8 +253,8 @@ static const struct keybinding keybindings[] = {
     { WLR_MODIFIER_LOGO, XKB_KEY_Escape, handle_exit },
     { WLR_MODIFIER_LOGO, XKB_KEY_Tab,    handle_cycle_window },
     { WLR_MODIFIER_LOGO, XKB_KEY_Return, handle_terminal },
-    { WLR_MODIFIER_LOGO, XKB_KEY_r,      handle_run },
-    { WLR_MODIFIER_LOGO, XKB_KEY_s,      handle_status },
+    { WLR_MODIFIER_LOGO, XKB_KEY_b,      handle_battery },
+    { WLR_MODIFIER_LOGO, XKB_KEY_t,      handle_time },
 
     /* media keys */
     { 0,                 XKB_KEY_XF86AudioRaiseVolume,  handle_volume_up },
